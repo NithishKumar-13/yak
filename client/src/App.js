1eb70b9829ca8b23cc902bd8a662b53b06ca4e61
Nithish Kumar <nithishpadma@gmail.com>
2021-07-24 19:24:58 +0530

    add Routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Route , Switch } from 'react-router-dom'
+import Join from './components/Join/Join'
+import Room from './components/Room/Room'
 
 const App = () => {
     return(
-        <div>
-            App
-        </div>
+       <Router>
+           <Switch>
+               <Route exact path='/' component={Join} />
+               <Route exact path='/room' component={Room} />
+           </Switch>
+       </Router>
     )
 }
 
