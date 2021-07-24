2421c0a12579c9d62998409f4b5a75a1efa2541b
Nithish Kumar <nithishpadma@gmail.com>
2021-07-24 19:26:26 +0530

    import App.scss in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route , Switch } from 'react-router-dom'
 import Join from './components/Join/Join'
 import Room from './components/Room/Room'
+import './App.scss'
 
 const App = () => {
     return(
