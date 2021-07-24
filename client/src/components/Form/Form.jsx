2d0f8425150edd822fd142b9e3831059df03081f
Nithish Kumar <nithishpadma@gmail.com>
2021-07-24 19:04:09 +0530

    add utilities.scss to Form

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,6 +1,6 @@
 import React , { useState , useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-//import '../../utilities.scss'
+import '../../utilities.scss'
 import './Form.scss'
 
 const Form = ({className}) => {
